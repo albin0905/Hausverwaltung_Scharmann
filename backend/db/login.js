84c9a2189e16accebbe6f45352b910e0b0b5b5de
albin <bajala20@htl-kaindorf.at>
2024-12-19 14:38:47 +0100

    backend changed

---
+++ backend/db/login.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const initMongoConnect = async () => {
+    try {
+        await mongoose.connect("mongodb://127.0.0.1:27017/hausverwaltung_scharmann", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB erfolgreich verbunden');
+    } catch (err) {
+        console.error('Fehler beim Verbinden mit MongoDB:', err);
+    }
+};
+
+const LoginSchema = new mongoose.Schema({
+    id: { type: Number, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true }
+}, { collection: 'login' }); // Explizit den Sammlungsnamen angeben
+
+const LoginModel = mongoose.model('Login', LoginSchema);
+
+module.exports = {
+    initMongoConnect,
+    Login: LoginModel
+};
