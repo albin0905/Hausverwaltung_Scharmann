854b8ca6c5d55b0a1e1b5d04642e4c3235aaf910
David Fink <findaa21@gmail.com>
2025-02-06 09:11:24 +0100

    Bild von Heli

--- frontend/src/views/layout/Impressum.tsx
+++ frontend/src/views/layout/Impressum.tsx
@@ -6,6 +6,7 @@ function Impressum() {
     return (
         <Container className={"marginSettings impressum"}>
             <p>Helmut Scharmann</p>
+            <img src={profilbild} alt="" style={{width:"30%"}}/>
             <p>Hier kommt der Text von Scharmann</p>
         </Container>
     );
