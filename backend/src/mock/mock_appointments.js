a1e7adec7d83efbe0c8b6c60cb13481e582e73d2
albin <bajala20@htl-kaindorf.at>
2025-03-06 15:04:12 +0100

    Backend & DB refactored

---
+++ backend/src/mock/mock_appointments.js
@@ -0,0 +1,18 @@
+const mockAppointments = [
+    {
+        id: 1,
+        userId: 1,
+        date: "2025-03-06",
+        time: "10:00",
+        description: "Besichtigung"
+    },
+    {
+        id: 2,
+        userId: 2,
+        date: "2025-03-07",
+        time: "14:00",
+        description: "Vertragsunterzeichnung"
+    }
+];
+
+module.exports = mockAppointments;
